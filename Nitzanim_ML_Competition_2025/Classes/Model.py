71ef6f75f35717a2b68802f1b7088270b50facd8
yuval1847 <yuval1847@gmail.com>
2025-02-21 18:57:27 +0200

    Label encodin, Normalization, Standardization and spliting data was finished

--- Nitzanim_ML_Competition_2025/Classes/Model.py
+++ Nitzanim_ML_Competition_2025/Classes/Model.py
@@ -2,11 +2,13 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.linear_model import LogisticRegression
+
 
 class Model:
     """
-    A class which represent the algorithem of the ML model for the Nitzanim ML Competition 2025.
+    A class which represent a ML model algorithem for the Nitzanim ML Competition 2025.
     This model contains some Supervised Learning models which together creates Ensemble Learning.
     """
 
@@ -17,9 +19,21 @@ class Model:
         # Variables which stores the data of the model (the train and test data) as DataFrame objects.
         self.train_data = train_data
         self.test_data = test_data
-
+        
+        # 2 lists of the features' names divided by representing qualitative or quantitative data types.
+        self.qualitative_features = ["WorkingCondition", "Education", "MaritalStatus", "Occupation", "Relationship", "Ethnicity", "Gender", "CountryOfOrigin", "MonthlyIncome"]
+        self.quantitative_features = ["Age", "FinalWeight", "YearsOfEducation", "InvestmentGains", "InvestmentLosses", "WeeklyWorkHours"]
+        
         # Removing the null values from the data
         self.Remove_Null_Values()
+
+        # Normalized data
+        self.normalized_train_data = self.Normalization(self.normalized_train_data)
+        self.normalized_test_data = self.Normalization(self.normalized_test_data)
+
+        # Standardized data
+        self.standardized_train_data = self.Standardization(self.standardized_train_data)
+        self.standardized_test_data =self.Standardization(self.standardized_test_data)
 
     
     # Preprocess functions:
@@ -49,26 +63,69 @@ class Model:
             axes[i].set_title(f"Bar Chart of {col}")
         plt.tight_layout()
         plt.show()
-    def Generate_Scatter_Plots(self, quantitative_features:list[str]):
+    def Generate_Box_Plots(self, quantitative_features:list[str]):
         # Input: A list of strings which represents a list of the quantitative features from the train data.
-        # Output: The function shows scatter plots of the train data quantitative features.
-        fig, axes = plt.subplots(len(quantitative_features), len(quantitative_features), figsize=(20, 20))
-        for i, col_x in enumerate(quantitative_features):
-            for j, col_y in enumerate(quantitative_features):
-                if i != j:
-                    sns.scatterplot(x=self.train_data[col_x], y=self.train_data[col_y], ax=axes[i, j], alpha=0.6)
-                    axes[i, j].set_title(f"Scatter Plot of {col_x} vs. {col_y}")
-                else:
-                    axes[i, j].axis("off")
+        # Output: The function shows box plots of the train data quantitative features.
+        fig, axes = plt.subplots(len(quantitative_features), 1, figsize=(8, 20))
+        for i, col in enumerate(quantitative_features):
+            sns.boxplot(x=self.train_data[col], ax=axes[i], palette="coolwarm")
+            axes[i].set_title(f"Box Plot of {col}")
         plt.tight_layout()
         plt.show()
     def Visualization(self):
         # Input: Nothing.
         # Output: The function creates 3 types of visualization based on the train data,
         # which are histograms, bar charts and scatter plots.
-        qualitative_features = ["WorkingCondition", "Education", "MaritalStatus", "Occupation", "Relationship", "Ethnicity", "Gender", "CountryOfOrigin"]
-        quantitative_features = ["Age", "FinalWeight", "YearsOfEducation", "InvestmentGains", "InvestmentLosses", "WeeklyWorkHours"]
-        self.Generate_Histograms(quantitative_features=quantitative_features)        
-        self.Generate_Bar_Charts(qualitative_features=qualitative_features)
-        self.Generate_Scatter_Plots(quantitative_features=quantitative_features) 
+        self.Generate_Histograms(quantitative_features=self.quantitative_features)        
+        self.Generate_Bar_Charts(qualitative_features=self.qualitative_features)
+        self.Generate_Box_Plots(quantitative_features=self.quantitative_features) 
     
+
+    # Label Encoding functions:
+    def Label_Encoding_Specific_Data(self, data:pd.core.frame.DataFrame):
+        # Input: DataFrame object which contains data.
+        # Output: The given DataFrame object after performing label encoding over it.
+        for i in self.qualitative_features:
+            data = LabelEncoder.fit_transform(data[i])
+        return data
+    def Label_Encoding(self):
+        # Input: Nothing.
+        # Output: The function performs label encoding over the train and test data.
+        self.train_data = self.Label_Encoding_Specific_Data(self.train_data)
+        self.test_data = self.Label_Encoding_Specific_Data(self.test_data)
+
+
+    # Normalization and Standardization functions:
+    def Normalization(self, data:pd.core.frame.DataFrame):
+        # Input: DataFrame object which contains data.
+        # Output: The given DataFrame object after performing normalization over it.
+        for i in self.quantitative_features:
+            if i != "MonthlyIncome":
+                data[i] = MinMaxScaler(feature_range=(0, 1)).fit_transform(data[[i]])
+        return data
+    def Standardization(self, data: pd.core.frame.DataFrame):
+        # Input: DataFrame object which contains data.
+        # Output: The given DataFrame object after performing standardization over it.
+        for i in self.quantitative_features:
+            if i != "MonthlyIncome":
+                data[i] = StandardScaler().fit_transform(data[[i]])
+        return data
+    
+
+    # Spliting data function:
+    def Spliting_Data(self, data:pd.core.frame.DataFrame):
+        # Input: DataFrame object which contains data.
+        # Output: The splited parts of the given data (the data without the MonthlyIncome column
+        # and the data with only the MonthlyIncome column).
+        return data.drop('MonthlyIncome', axis=1), data['MonthlyIncome']
+
+
+    # Supervised Models functions:
+    def Logistic_Regression_Model(self):
+        # Input: Nothing.
+        # Output: The function returns the 
+
+    def Learning(self):
+        # Input: Nothing.
+        # Output: The function use all the learning models and returns a concluded result.
+        
