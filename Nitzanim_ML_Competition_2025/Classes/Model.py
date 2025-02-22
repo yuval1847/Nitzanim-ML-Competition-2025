7558aeea1140fb8dafb75a1de7750911aded4e8c
yuval1847 <yuval1847@gmail.com>
2025-02-22 21:15:47 +0200

    Models added! Perform testing

--- Nitzanim_ML_Competition_2025/Classes/Model.py
+++ Nitzanim_ML_Competition_2025/Classes/Model.py
@@ -4,12 +4,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 
 class Model:
     """
     A class which represent a ML model algorithem for the Nitzanim ML Competition 2025.
-    This model contains some Supervised Learning models which together creates Ensemble Learning.
+    This model contains some Supervised Learning models which together creates Ensemble Learning Model.
     """
 
     # Constractor:
@@ -121,11 +125,58 @@ class Model:
 
 
     # Supervised Models functions:
-    def Logistic_Regression_Model(self):
-        # Input: Nothing.
-        # Output: The function returns the 
+    def Logistic_Regression_Model(self, train_data_x:pd.core.frame.DataFrame, train_data_y:pd.core.frame.DataFrame):
+        # Input: DataFrame objects which represent the 2 splited parts of the train data.
+        # Output: The function returns a Logistic Regression model after training it.
+        logistic_regression_model = LogisticRegression(max_iter=1000)
+        logistic_regression_model.fit(train_data_x, train_data_y)
+        return logistic_regression_model
+    def Random_Forest_Model(self, train_data_x:pd.core.frame.DataFrame, train_data_y:pd.core.frame.DataFrame):
+        # Input: DataFrame objects which represent the 2 splited parts of the train data.
+        # Output: The function returns a Random Forest model after training it.
+        random_forest_model = RandomForestClassifier(n_estimators=100, random_state=42)
+        random_forest_model.fit(train_data_x, train_data_y)
+        return random_forest_model
+    def KNN_Model(self, standardized_train_data_x:pd.core.frame.DataFrame, standardized_train_data_y:pd.core.frame.DataFrame):
+        # Input: DataFrame objects which represent the 2 splited parts of the standardized train data.
+        # Output: The function returns a KNN model after training it.
+        knn_model = KNeighborsClassifier(n_neighbors=5)
+        knn_model.fit(standardized_train_data_x, standardized_train_data_y)
+        return knn_model
+    def Gaussian_Naive_Bayes(self, train_data_x:pd.core.frame.DataFrame, train_data_y:pd.core.frame.DataFrame):
+        # Input: DataFrame objects which represent the 2 splited parts of the train data.
+        # Output: The function returns a Gaussian Naive Bayes model after training it.
+        gaussian_naive_bayes = GaussianNB()
+        gaussian_naive_bayes.fit(train_data_x, train_data_y)
+        return gaussian_naive_bayes
+    def SVM_Model(self, standardized_train_data_x:pd.core.frame.DataFrame, standardized_train_data_y:pd.core.frame.DataFrame):
+        # Input: DataFrame objects which represent the 2 splited parts of the train data.
+        # Output: The function returns a Support Vector Machine model after training it.
+        svm_model = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
+        svm_model.fit(standardized_train_data_x, standardized_train_data_y)
+        return svm_model
 
-    def Learning(self):
+
+    # Training function:
+    def Training(self):
         # Input: Nothing.
-        # Output: The function use all the learning models and returns a concluded result.
-        
+        # Output: A list of the the models after inserting them the training data.
+        train_data_x, train_data_y = self.Spliting_Data(self.train_data)
+        normalized_train_data_x, normalized_train_data_y = self.Spliting_Data(self.normalized_train_data)
+        standardized_train_data_x, standardized_train_data_y = self.Spliting_Data(self.standardized_train_data)
+
+        models = [
+            self.Logistic_Regression_Model(train_data_x, train_data_y),
+            self.Random_Forest_Model(train_data_x, train_data_y),
+            self.KNN_Model(standardized_train_data_x, standardized_train_data_y),
+            self.Gaussian_Naive_Bayes(train_data_x, train_data_y),
+            self.SVM_Model(standardized_train_data_x, standardized_train_data_y)
+        ]
+
+        return models
+
+
+    # Testing function:
+    def Testing(self, models:list):
+        # Input: A list of the trained models.
+        # Output: 
