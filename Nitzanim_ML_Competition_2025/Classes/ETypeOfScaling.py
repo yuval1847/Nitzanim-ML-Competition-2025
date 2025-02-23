d60ce37f4cc7b946581e0c4ebeebf9ba3e949fbd
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2025-02-23 23:39:55 +0200

    Delete Nitzanim_ML_Competition_2025/Classes/ETypeOfScaling.py

--- Nitzanim_ML_Competition_2025/Classes/ETypeOfScaling.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class ETypeOfScaling(Enum):
-    """
-    An enum which contains the type of scaling
-    """
-    NO_SCALING = 1
-    NORMALIZATION = 2
-    STANDARDIZATION = 3
